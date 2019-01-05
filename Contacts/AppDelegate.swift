941e8f05924e80ff07a4ab4bb8e2cce8ce2656d1
tejashwini <tejashwini@tejashwinis-Mac.local>
2019-01-05 00:58:23 -0800

    Initial Commit of the Contacts App.

--- Contacts/AppDelegate.swift
+++ Contacts/AppDelegate.swift
@@ -13,10 +13,12 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print("Application did finish launch called")
+        
         return true
     }
 
